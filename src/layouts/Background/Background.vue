a1244f788ba92bb49707ca9682d09cdde31e7e9f
Piyotr-K <piyotrkao1998@outlook.com>
2020-09-16 16:37:58 -0700

    HomePage
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

---
+++ src/layouts/Background/Background.vue
@@ -0,0 +1,4 @@
+<template src="./Background.html"></template>
+<script lang="ts" src="./Background.ts"></script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" src="./Background.scss"></style>
