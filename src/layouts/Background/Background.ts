30ce1b83804ebad0d436f390838bce2a44330a63
Piyotr-K <piyotrkao1998@outlook.com>
2020-09-17 15:03:28 -0700

    Added dynamic background changing and added hide to header
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- src/layouts/Background/Background.ts
+++ src/layouts/Background/Background.ts
@@ -3,12 +3,19 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class Background extends Vue {
   dToggle = false;
-  imageUrl = "";
-  created() {
+  imageUrl = require("../../assets/main_back.png");
+
+  created() 
+  {
     console.log(this.$router.currentRoute.path);
     const route = this.$router.currentRoute.path;
-    if (route == "/contact") {
+    if (route == "/contact")
+    {
       this.dToggle = true;
+    }
+    if (route == "/about")
+    {
+      this.imageUrl = require("../../assets/about_back.jpg");
     }
   }
 }
--- src/layouts/Header/Header.ts
+++ src/layouts/Header/Header.ts
@@ -7,4 +7,15 @@ import Dropdown from "bp-vuejs-dropdown";
   }
 })
 @Component
-export default class Header extends Vue {}
+export default class Header extends Vue {
+  iToggle = false;
+  created()
+  {
+    console.log(this.$router.currentRoute.path);
+    const route = this.$router.currentRoute.path;
+    if (route == "/")
+    {
+      this.iToggle = true;
+    }
+  }
+}
