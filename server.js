6543ba4ad83f269841d6d3fce23cc0503ffb34da
Piyotr-K <piyotrkao1998@outlook.com>
2020-09-16 23:39:27 -0700

    Quotation change
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const serveStatic = require("serve-static")
-const path = require('path');
+const path = require("path");
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
