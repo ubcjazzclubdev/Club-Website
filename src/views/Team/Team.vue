bf14da8fbc75aa37b665332f6e71198e403fd76f
Piyotr-K <piyotrkao1998@outlook.com>
2020-09-21 01:46:40 -0700

    Added routes to router and header
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

---
+++ src/views/Team/Team.vue
@@ -0,0 +1,4 @@
+<template src="./Team.html"></template>
+<script lang="ts" src="./Team.ts"></script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" src="./Team.scss"></style>
