fca48e60b6a7be1bdb006745987a9d66f83dcafe
Piyotr-K <piyotrkao1998@outlook.com>
2020-09-24 21:19:37 -0700

    GalleryPage inital commit
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,7 @@ import Contact from "./views/Contactus/Contact.vue";
 import Team from "./views/Team/Team.vue";
 import Involved from "./views/Involvement/Involvement.vue";
 import Events from "./views/Events/Events.vue";
+import Gallery from "./views/Gallery/Gallery.vue";
 
 Vue.use(VueRouter);
 
@@ -17,13 +18,11 @@ const routes = [
   { path: "/about", component: About },
   { path: "/team", component: Team},
   { path: "/involvement", component: Involved},
+  { path: "/gallery", component: Gallery },
   { path: "/contact", component: Contact },
   { path: "/events", component: Events }
 ];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = new VueRouter({
   routes, // short for `routes: routes`
   mode: "history" // get rid of the `#` symbol
