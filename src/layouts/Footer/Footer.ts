63b46a27e8176cf17fe5a687120fe6e6da89d86f
Piyotr-K <piyotrkao1998@outlook.com>
2021-01-06 21:32:30 -0800

    Footer Newsletter Signup Implementation
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- src/layouts/Footer/Footer.ts
+++ src/layouts/Footer/Footer.ts
@@ -1,5 +1,9 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
+interface FormElements extends HTMLFormControlsCollection {
+  email : HTMLInputElement;
+}
+
 @Component
 export default class Footer extends Vue {
   scrollToTop(): void {
@@ -10,5 +14,23 @@ export default class Footer extends Vue {
       block: "start",
       inline: "nearest"
     });
+  }
+
+  /*
+   * Modified version of the contact us form.
+   * Uses name field on google form as 
+   */
+  staticSubmit(e : Event): void {
+    const form : HTMLFormElement = e.target as HTMLFormElement;
+    const elements : FormElements = form.elements as FormElements;
+    // console.log(elements.email.value);
+    form.action = "https://docs.google.com/forms/d/e/1FAIpQLSdANikULQYuHmxoWLfWOw2t2a1vNcRf6bSbUXk8OSR613sTpw/formResponse?";
+    if (elements.email.value != "") {
+      form.submit();
+      form.reset();
+      elements.email.placeholder = "Sent!";
+    } else {
+      elements.email.placeholder = "Empty Email!";
+    }
   }
 }
