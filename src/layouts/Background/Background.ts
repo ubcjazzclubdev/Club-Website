8d9b447deedf7c490be0f51dd79ac9b0da934956
Piyotr-K <piyotrkao1998@outlook.com>
2021-01-21 16:59:14 -0800

    v1 of assest folder reorganization
    Signed-off-by: Piyotr-K <piyotrkao1998@outlook.com>

--- src/layouts/Background/Background.ts
+++ src/layouts/Background/Background.ts
@@ -3,7 +3,7 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class Background extends Vue {
   dToggle = false;
-  imageUrl = require("../../assets/main_back.png");
+  imageUrl = require("../../assets/images/page_bgs/main_back.png");
 
   created() {
     const route = this.$router.currentRoute.path;
@@ -11,19 +11,19 @@ export default class Background extends Vue {
       this.dToggle = true;
     }
     if (route == "/about") {
-      this.imageUrl = require("../../assets/about_back.jpg");
+      this.imageUrl = require("../../assets/images/page_bgs/about_back.jpg");
     }
     if (route == "/team") {
-      this.imageUrl = require("../../assets/team_back.jpg");
+      this.imageUrl = require("../../assets/images/page_bgs/team_back.jpg");
     }
     if (route == "/involvement") {
       this.imageUrl = require("../../assets/images/imagine_day_1.jpg");
     }
     if (route == "/gallery") {
-      this.imageUrl = require("../../assets/gallery_back.jpg");
+      this.imageUrl = require("../../assets/images/page_bgs/gallery_back.jpg");
     }
     if (route == "/events") {
-      this.imageUrl = require("../../assets/events_back.jpg");
+      this.imageUrl = require("../../assets/images/page_bgs/events_back.jpg");
     }
   }
 }
