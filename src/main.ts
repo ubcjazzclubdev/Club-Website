9d5d63b8987f52c2c68d299f7b2370a401c382ad
dylbrown <dylanmb193@gmail.com>
2023-07-12 01:04:58 -0700

    Add fontawesome and update footer

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,31 @@ import App from "./App.vue";
 import Router from "./router";
 import mitt, { Emitter } from "mitt";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faDiscord, faFacebook, faInstagram, faEnvelope, faLocationDot);
+
 type Events = {
   "modal-open": string;
   "unhide-nav": boolean;
 };
 
 export const emitter: Emitter<Events> = mitt<Events>();
-const app = createApp(App).use(Router);
+const app = createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(Router);
 app.config.globalProperties.emitter = emitter;
 app.mount("#app");
 /*export const bus = new Vue();
